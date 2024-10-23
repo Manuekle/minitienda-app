8c74a285accde1bd775cba9ac7ed6c92f302e3d7
1002820487 <manuel.erazo@estudiante.fup.edu.co>
2024-10-23 16:01:31 -0500

    metodos logic

---
+++ MinitiendaWebApp/Logic/CategoryLog.cs
@@ -0,0 +1,18 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Logic
+{
+    public class CategoryLog
+    {
+        CategoryDat objCat = new CategoryDat();
+        public DataSet showCategories() {
+            return objCat.showCategories();
+        }
+
+    }
+}
